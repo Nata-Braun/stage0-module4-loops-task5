5a3a0d6491d96e73f434741fc8ff4f0bcbbfb92c
Nata Braun <pushistik2030@gmail.com>
2023-02-06 22:43:24 +0300

    A half of task

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,51 @@
 package school.mjc.stage0.loops.task5;
 
+import java.io.PrintStream;
+
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+
+
+
+    public  void printHourglassOfGivenSize(int height) {
+        if (height <= 0) {
+            return;
+        }
+        int middle = height/2+1;
+        for (int i=1; i<=middle; i++) {
+            String newLine = "";
+
+            for (int k = 1; k <= height; k++) {
+               if (k >= i && k<= height-i+1) {
+                    newLine = newLine + "8";
+                } else {
+                    newLine = newLine + " ";
+                }
+
+            }
+            if (!newLine.contains("8")){
+                continue;
+            }
+            System.out.println(newLine);
+        }
+
+        for (int i=middle-1; i > 0; i--) {
+            String newLine = "";
+
+            for (int k = 1; k <= height; k++) {
+                if (k >= i && k<= height-i+1) {
+                    newLine = newLine + "8";
+                } else {
+                    newLine = newLine + " ";
+                }
+            }
+            if (!newLine.contains("8"))
+                continue;
+
+            System.out.println(newLine);
+        }
+
+
+
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
+    public static void main (String[]args){
+        printRectangleFrom8s(6,8);
+    }
+    public static void printRectangleFrom8s(int length, int height){
+        if (height <= 0) {
+            return;
+        }
+        for (int i=1; i<=height; i++){
+            String newLine = "";
+            if (i==1 || i==height) {
+                for (int j = 1; j <= length; j++) {
+                    newLine = newLine + "8";
+                }
+            } else {
+                for (int k = 1; k <= length; k++)
+                    if (k == 1 || k == length) {
+                        newLine = newLine + "8";
+                    } else {
+                        newLine = newLine + " ";
+                    }
+                }
+            System.out.println(newLine);
+        }
 
     }
 }
